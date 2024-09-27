304f66431f05ace9efa85067b5e602b24e0c8695
chendechuan <chen0802@qq.com>
2024-09-27 14:56:10 +0800

    1. 将事件处理函数作为 props 传递；

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-export default function Button() {
+function Button({ onClick, children }) {
+  return <button onClick={onClick}>{children}</button>;
+}
+
+function PlayButton({ movieName }) {
+  function handlePlayClick() {
+    alert(`正在播放 ${movieName}！`);
+  }
+
+  return <Button onClick={handlePlayClick}>播放 "{movieName}"</Button>;
+}
+
+function UploadButton() {
+  return <Button onClick={() => alert("正在上传！")}>上传图片</Button>;
+}
+
+export default function Toolbar() {
   return (
-    <button>
-      未绑定任何事件
-    </button>
+    <div>
+      <PlayButton movieName="魔女宅急便" />
+      <UploadButton />
+    </div>
   );
 }
